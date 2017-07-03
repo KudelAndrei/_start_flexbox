d9b9ff798d06f47a89755e7de06b8ae409044319
kudelandrei <kudel1995@mail.ru>
2017-07-03 20:16:24 +0300

    start

---
+++ app/js/common.js
@@ -0,0 +1,11 @@
+/* For preloader */
+$(window).on('load', function(){
+	//$('.preloader').delay(500).fadeOut('slow');
+});
+
+/* scripts ready */
+$(document).ready(function(){
+
+	console.log('ready');
+	
+});
